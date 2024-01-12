ec00a9e7adf7bc91ed26ba1b953d0a479f18fb57
Abenezer Seifu <abenezerseifu123@gmail.com>
2024-01-12 13:59:53 +0300

    Customer dashboard and modularized fetches

--- FrontEnd/fetch/BookingsFetch.js
+++ FrontEnd/fetch/BookingsFetch.js
@@ -1,62 +1,9 @@
 var bookingsAPI = 'http://localhost:9000/bookings';
 var technicianBookingsAPI = 'http://localhost:9000/bookings/technician';
 var customerBookingsAPI = 'http://localhost:9000/bookings/customer';
-var technicianAPI = 'http://localhost:9000/technician';
-var addNewBooking = document.getElementById("new-booking");
-var bookingsParent = document.getElementById("bookings");
-var allContainer = document.getElementsByClassName("all-container")[0];
-if (allContainer) {
-    allContainer.id = "container-" + localStorage.getItem("individualTech");
-}
-var techProfile = document.getElementsByClassName("profile-detail")[0];
-//Create Booking
-//Create HTML tag for booking
-// function createBooking(booking: any, technician: any, customer: any): HTMLElement {
-//     const bookingDiv = document.createElement("div");
-//     bookingDiv.classList.add("booking-"+booking.id);
-//     const bookingInfoDiv = document.createElement("div");
-//     bookingInfoDiv.classList.add("booking")
-//     bookingDiv.appendChild(bookingInfoDiv)
-//     let role = customer
-//     let userRole = localStorage.getItem('userRole')
-//     if (userRole == "customer") {
-//         const editButton = document.createElement("button");
-//         editButton.innerText = "Edit"
-//         bookingDiv.appendChild(editButton)
-//         const saveButton = document.createElement("button");
-//         saveButton.innerText = "Save Changes"
-//         bookingDiv.appendChild(saveButton)        
-//         const deleteButton = document.createElement("button");
-//         deleteButton.innerText = "Save Changes"
-//         bookingDiv.appendChild(deleteButton)
-//     } else if (userRole == "technician") {
-//         role = technician;
-//         const acceptButton = document.createElement("button");
-//         acceptButton.innerText = "Accept"
-//         bookingDiv.appendChild(acceptButton)
-//         const declineButton = document.createElement("button");
-//         declineButton.innerText = "Decline"
-//         bookingDiv.appendChild(declineButton)
-//         const servicedButton = document.createElement("button");
-//         servicedButton.innerText = "Serviced"
-//         bookingDiv.appendChild(servicedButton)
-//     }
-//     let bookingInfo = `<h3>${booking.serviceNeeded}</h3>
-//                         <div class="technician-info">
-//                             <h4>Booked with<h4>
-//                             <p>Name: ${role.fullName}</p>
-//                             <p>E-Mail: ${role.email}</p>
-//                             <p>Phone No: ${role.phone}</p>
-//                         </div>
-//                         <p>Problem description: ${booking.problemDescription}</p>
-//                         <p>Service date: ${booking.serviceDate}</p>
-//                         <p>Location: ${booking.serviceLocation}<p>
-//                         <p>Status<p>`
-//     bookingInfoDiv.innerHTML = bookingInfo;
-//     return bookingDiv
-// }
+
 //Post booking on server
-function postBookingToServer() {
+export function postBookingToServer(bookedForTemp) {
     var bookedByTemp;
     if (localStorage.getItem("userId")) {
         bookedByTemp = parseInt(localStorage.getItem("userId"));
@@ -64,11 +11,8 @@ function postBookingToServer() {
     else {
         return;
     }
-    var bookedForTemp;
-    if (allContainer) {
-        bookedForTemp = parseInt(allContainer.id.split("-")[1]);
-    }
-    else {
+    
+    if (!(bookedForTemp)) {
         return;
     }
     var serviceDateTemp = document.getElementById("serviceDate");
@@ -88,11 +32,13 @@ function postBookingToServer() {
     })
         .then(function (response) { return response.json(); })
         .then(function (data) {
+
         if ("error" in data) {
             alert("Please you have to sign in first to book");
             return;
         }
         console.log(data);
+        alert("Booked Successfully!")
         window.location.reload();
     })
         .catch(function (error) { return console.error("Error adding booking!", error); });
@@ -110,25 +56,25 @@ function readBookingsOfTechnicianFromServer() {
         .then(function (response) { return response.json(); })
         .then(function (data) {
         console.log(data);
-        // createBooking(data, tempTechnician, tempCustomer);
     })
         .catch(function (error) { return console.error("Error fetching bookings:", error); });
 }
 //Get bookings specific to customer
-function readBookingsOfCustomerFromServer() {
+export async function readBookingsOfCustomerFromServer() {
     var customerURL = "".concat(customerBookingsAPI, "/").concat(localStorage.getItem("userId"));
-    fetch(customerURL, {
-        method: 'GET',
-        headers: {
-            'Authorization': "Bearer ".concat(localStorage.getItem('jwtToken'))
-        }
-    })
-        .then(function (response) { return response.json(); })
-        .then(function (data) {
+    try {
+        const response = await fetch(customerURL, {
+            method: 'GET',
+            headers: {
+                'Authorization': "Bearer ".concat(localStorage.getItem('jwtToken'))
+            }
+        })
+        const data = await response.json()
         console.log(data);
-        // createBooking(data, tempTechnician, tempCustomer);
-    })
-        .catch(function (error) { return console.error("Error fetching bookings:", error); });
+        return data
+    }
+
+    catch(error) { return console.error("Error fetching bookings:", error); };
 }
 //Update booking
 //Update booking on html
@@ -157,9 +103,8 @@ function updateBookingOnServer() {
         .catch(function (error) { return console.error("Error updating booking!", error); });
 }
 //Delete booking
-function deleteBookingFromServer() {
-    var deleteUrl = "".concat(bookingsAPI, "/").concat(1);
-    var bookingToBeDeleted = document.getElementById("");
+export function deleteBookingFromServer(id) {
+    var deleteUrl = "".concat(bookingsAPI, "/").concat(id);
     fetch(deleteUrl, {
         method: "DELETE",
         headers: {
@@ -169,34 +114,9 @@ function deleteBookingFromServer() {
         .then(function (response) { return response.json(); })
         .then(function (data) {
         console.log(data);
-        if (bookingToBeDeleted) {
-            bookingToBeDeleted.remove();
-        }
     })
         .catch(function (error) { return console.error("Error deleting booking!", error); });
 }
-var submitBooking = document.getElementById("submitBooking");
-if (submitBooking) {
-    submitBooking.addEventListener("click", postBookingToServer);
-}
-function fetchTechnicianProfile() {
-    var techApi = "".concat(technicianAPI, "/").concat(localStorage.getItem("individualTech"));
-    fetch(techApi, {
-        method: 'GET',
-        headers: {
-            'Authorization': "Bearer ".concat(localStorage.getItem('jwtToken'))
-        }
-    })
-        .then(function (response) { return response.json(); })
-        .then(function (data) {
-        console.log(data);
-        showTechnicianProfile(data);
-    })
-        .catch(function (error) { return console.error("Error fetching technician:", error); });
-}
-function showTechnicianProfile(tech) {
-    if (techProfile) {
-        techProfile.innerHTML = "<h2>".concat(tech.fullName, "</h2>\n        <p><strong>Email</strong>: ").concat(tech.email, "</p>\n        <p><strong>Phone</strong> : ").concat(tech.phone, "</p>\n        <p><strong>Skills</strong> : ").concat(tech.skills, "</p>\n        <p><strong>Experience</strong> : ").concat(tech.experience, "</p>\n        <p><strong>Education Level</strong> : ").concat(tech.educationLevel, "</p>\n        <p><strong>Available Location</strong> : ").concat(tech.availableLocation, "</p>\n        <p>\n        <strong>Additional Bio</strong> : ").concat(tech.additionalBio, "\n        </p>");
-    }
-}
-fetchTechnicianProfile();
+
+
+
